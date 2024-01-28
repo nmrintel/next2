8a2ddef380b0e0eb5a5e6591adbac0e3d77aa83e
nmrintel <sakedaisuki3.14@gmail.com>
2024-01-29 05:48:57 +0900

    ログイン機構

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+// ログインとユーザー登録の画面の表示がおかしくなるのでglobals.cssは読み込まない
+// import '../styles/globals.css'
+// ↓ これを追加してBootstrapで読み込む
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react"
+import { AuthProvider } from "../context/AuthContext"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: { Component: React.ElementType, pageProps: any }) {
+  return (<AuthProvider>
+    <Component {...pageProps} />
+  </AuthProvider>)
 }
+
+export default MyApp
---
+++ pages/login.js
@@ -0,0 +1,82 @@
+import styles from '../styles/Home.module.css'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { Col, Container, Form, FormGroup, Input, Label, Row, Button } from "reactstrap";
+import { useState } from 'react';
+import Link from 'next/link';
+
+
+export default function Login() {
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const doLogin = () => {
+    const auth = getAuth();
+    
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        
+        const user = userCredential.user;
+        
+        alert('ログインOK!');
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('ログイン失敗')
+      });
+  }
+  return (
+    <div className={styles.card}>
+      <h1>ログイン</h1>
+      <div style={{ paddingBottom: "1rem" }}>
+        <Form>
+          <FormGroup>
+            <Label>
+              メールアドレス：
+            </Label>
+            <Input
+              type="email"
+              name="email"
+              style={{ height: 50, fontSize: "1.2rem" }}
+              
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>
+              パスワード：
+            </Label>
+            <Input
+              type="password"
+              name="password"
+              style={{ height: 50, fontSize: "1.2rem" }}
+              
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </FormGroup>
+          <Button
+            style={{ width: 220 }}
+            color="primary"
+            
+            onClick={() => {
+              doLogin();
+            }}
+          >
+            ログイン
+          </Button>
+        </Form>
+      </div>
+      <div style={{flexbox}}>
+      <Link
+        href="/forgot_password">
+        パスワードを忘れた場合
+      </Link>
+      <Link
+        href="/register">
+          新規登録
+      </Link>
+      </div>
+    </div>
+  )
+}
